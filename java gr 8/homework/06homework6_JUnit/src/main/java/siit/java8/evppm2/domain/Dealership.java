5a5fb05b40596ee3cf6dc833775b4d81c033ca8e
narcisal <narcisalontis@gmail.com>
2017-09-30 17:30:55 +0300

    addend homework6-JUnit

---
+++ java gr 8/homework/06homework6_JUnit/src/main/java/siit/java8/evppm2/domain/Dealership.java
@@ -0,0 +1,21 @@
+package siit.java8.evppm2.domain;
+
+
+
+/**
+ * Created by Narci on 8/26/2017
+ */
+public class Dealership {
+
+    static EvCar[] evCars = new EvCar[ 9 ];
+
+    public Dealership ( ) {
+    }
+
+    public Dealership ( String s, Stock[] stocks ) {
+    }
+
+    public static void initEvCars ( int position, int price, String manufacturer, String model ) {
+
+    }
+}
---
+++ java gr 8/homework/06homework6_JUnit/src/main/java/siit/java8/evppm2/domain/SearchCriteria.java
@@ -0,0 +1,9 @@
+package siit.java8.evppm2.domain;
+
+/**
+ * Created by Narci on 10/09/2017
+ */
+public interface SearchCriteria {
+
+    String buildCriteria ( Object[] characteristics );
+}
