a33483419cd3e7524f503050356d045bbdc56793
narcisal <narcisalontis@gmail.com>
2017-11-12 13:47:55 +0200

    I added homework 8-Exceptions & IO-Milestone5&7-FEV6, FEV7, FFEV10, FEV11, FEV12, NFEV3

---
+++ java gr 8/homework/hm8.Milestone5&T.ExceptionIO/src/test/java/siit/gr8/narci/milestone/TestPurchaseOutOfStockException.java
@@ -0,0 +1,37 @@
+package siit.gr8.narci.milestone;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class TestPurchaseOutOfStockException {
+    @Test
+    public void testCustomerPurchaseException ( ) throws Exception {
+    }
+
+{
+
+        // given
+
+        Customer customer = new Customer ( "Narcisa Lontis", 70000, 0, "Volkswagen", "e-UP",
+                true, true, true );
+
+
+        Dealership dealership = new Dealership ( "Dealer", 40000, false, false, 0 );
+
+        ArrayList <Car> cars = new ArrayList <> ();
+
+        dealership.cars.add ( new Car ( "Volkswagen", "e-UP", true, 2000, 25000,
+                0, 60, 90, true ) );
+
+
+
+
+        // then
+        customer.buyCarAtFullPrice ( cars, dealership );
+        dealership.cars.get ( 0 ).setDealerCarStock (0);
+
+
+
+    }
+}
