ecb57e9e9293f36b9c61bcf41c4a78530add2e99
narcisal <narcisalontis@gmail.com>
2017-11-12 13:43:05 +0200

    I added homework 7 refactored , Milestone4-Object Containers Assignment-FEV3, FEV4, FEV5, EV8, FEV9, NFEV2

---
+++ java gr 8/homework/hm7.Milestone4.ObjContAssig/src/test/java/siit/gr8/narci/milestone/TestCustomerRequestForGreenBonus.java
@@ -0,0 +1,40 @@
+package siit.gr8.narci.milestone;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestCustomerRequestForGreenBonus {
+    // FEV5 - Customer requests Green Bonus for an ev from the dealership.
+
+    @Test
+    public void testCustomerRequestForGreenBonus ( ) {
+
+        // given
+        Customer customer = new Customer ( "Narcisa Lontis", 70000, 1, "Wolkswagen", "e-UP",
+                true, true );
+
+
+        Dealership dealership = new Dealership ( "Dealer", 40000, true, false, 0 );
+        GreenBonusProgram greenBonusProgram = new GreenBonusProgram ( 20000, 100 );
+        ArrayList <Car> cars = new ArrayList <> ();
+
+        dealership.cars.add ( new Car ( "Wolkswagen", "e-Golf", true, 2017
+                , 38000, 1, 70, 80, true ) );
+        // when
+        customer.customerRequestForGreenBonus ( cars, dealership );
+        // then
+        ArrayList <Car> expectedCar = new ArrayList <Car> ();
+        expectedCar.add ( new Car ( "Wolkswagen", "e-Golf", true, 2017
+                , 38000, 1, 70, 80, true ) );
+        assertEquals ( expectedCar.get ( 0 ), dealership.cars.get ( 0 ) );
+        boolean expectedRequestResult = true;
+        assertTrue ( expectedRequestResult = true );
+        assertEquals ( expectedRequestResult, customer.getCustomerRequestForGreenBonus () );
+    }
+}
+
+
---
+++ java gr 8/homework/hm7.Milestone4.ObjContAssig/src/test/java/siit/gr8/narci/milestone/TestCustomerUsesGreenBonusOffer.java
@@ -0,0 +1,40 @@
+package siit.gr8.narci.milestone;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class TestCustomerUsesGreenBonusOffer {
+// FEV9 - Customer uses the Green Bonus Offer to purchase the ev
+
+    @Test
+    public void testCustomerUsesGreenBonusToBuyElectricVehicle ( ) {
+
+        // given
+        Customer customer = new Customer ( "Narcisa Lontis", 70000, 1, "Wolkswagen", "e-UP",
+                true, true );
+        Dealership dealership = new Dealership ( "Dealer", 40000, true, false, 0 );
+        GreenBonusProgram greenBonusProgram = new GreenBonusProgram ( 20000, 100 );
+        ArrayList <Car> cars = new ArrayList <> ();
+        dealership.cars.add ( new Car ( "Wolkswagen", "e-UP", false, 2000, 25000,
+                2, 60, 90, true ) );
+
+
+        // when
+
+        customer.customerRequestForGreenBonus ( cars, dealership );
+        dealership.sendGreenBonusNotification ( customer, greenBonusProgram );
+        customer.buyWithGreenBonus ( cars, dealership );
+        float expectedPrice = dealership.cars.get ( 0 ).getPrice ()-10000;
+
+
+        // then
+
+        boolean offerValue = true;
+        assertEquals ( offerValue, customer.getCustomerNotificationForGreenBonus () );
+
+    }
+}
